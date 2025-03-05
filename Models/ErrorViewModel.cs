e9ef26dd2fcd12f3c9de313373a990b14240bb9e
furblod <furkanvatansever4@gmail.com>
2025-03-05 12:55:37 +0300

    firts commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace cansaraciye_ecommerce.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}

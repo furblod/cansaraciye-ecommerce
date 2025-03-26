c187d74658b3e010d0c2c230d611266c06834fbf
furblod <furkanvatansever4@gmail.com>
2025-03-26 16:32:22 +0300

    orders tasarım

--- obj/Debug/net8.0/cansaraciye-ecommerce.AssemblyInfo.cs
+++ obj/Debug/net8.0/cansaraciye-ecommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cansaraciye-ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9314c56d9ad5a5f694325690c6310aaf55aac9d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee6f641a45652c1d53ed8bcc81be09fe177b8e74")]
 [assembly: System.Reflection.AssemblyProductAttribute("cansaraciye-ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cansaraciye-ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
